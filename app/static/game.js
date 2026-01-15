73c2488aff8413c32f4e909a9dee3a41bd7ba492
Alvin Sze <chateaudelete@gmail.com>
2026-01-14 22:20:44 -0500

    wow big update i love images hooray

--- app/static/game.js
+++ app/static/game.js
@@ -4,7 +4,13 @@
 // p02
 // 2026-01-16f
 
-var dead = false
+const spike = new Image()
+spike.src = "../static/images/spike.png"
+
+const logo = new Image()
+logo.src = "https://media.tenor.com/ifD1GaekwpoAAAAj/uma-musume-agnes-tachyon.gif"
+
+var dead = true
 
 var positionY = 0
 var velocityY = 0
@@ -24,6 +30,53 @@ var orbing = false
 
 var scrollSpeed = 8
 
+var buttons = []
+
+var button = (function () {
+  function button(text, color, x, y, width, height){
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+    this.color = color
+    this.text = text
+  }
+
+  button.prototype.draw = function(ctx){
+    ctx.fillStyle = this.color
+    ctx.fillRect(this.x, this.y, this.width, this.height)
+    ctx.fillStyle = "black"
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.font = '16px arial';
+    ctx.fillText(this.text, this.x + this.width / 2, this.y + this.height / 2, this.width);
+  }
+
+  button.prototype.mousehover = function(mouseX, mouseY){
+    return !(mouseX < this.x || mouseX > this.x + this.width || mouseY > this.y + this.height || mouseY < this.y)
+  }
+
+  return button
+}())
+
+function reset(){
+  buttons = []
+  dead = false;
+  update()
+}
+
+function startScreen(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var level1 = new button("levelOne", "orange", canvas.width/10, canvas.height/10, 80, 45)
+  level1.onClick = function () {return reset()}
+  buttons.push(level1)
+
+  for (var i = 0; i < buttons.length; i ++) {
+    return buttons[i].draw(ctx)
+  }
+
+}
+
 var objects = [
   {tag: "spike", x: 1000, y: floorLocation + 10, width: 12, height: 30},
   {tag: "spike", x: 1050, y: floorLocation + 10, width: 12, height: 30},
@@ -33,10 +86,9 @@ var objects = [
 ]
 
 function update() {
-  if (dead) {
-    return;
+  if (dead){
+    return
   }
-
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.fillStyle = "cyan"
@@ -48,8 +100,7 @@ function update() {
     let positionX = canvas.width / 10
 
     if (obj.tag == "spike") {
-      ctx.fillStyle = "red"
-      ctx.fillRect(obj.x, obj.y, obj.width, obj.height)
+      ctx.drawImage(spike, obj.x, obj.y, blocksize/2, blocksize/2)
 
       if (positionX + blocksize > obj.x &&
           positionX < obj.x + obj.width &&
@@ -98,6 +149,9 @@ function update() {
 if (!dead) {
   update();
 }
+else {
+  startScreen();
+}
 
 function jump() {
   if (grounded || orbing) {
@@ -109,15 +163,27 @@ document.addEventListener("keydown", e => {
   if (e.code === "Space" || e.code === "KeyW" || e.code === "ArrowUp") jump();
 });
 
-document.addEventListener("mousedown", e => {
-  if (e.button === 0) jump();
+canvas.addEventListener("click", function(e) {
+  if (dead){
+    var mouseX = e.pageX - (canvas.clientLeft + canvas.offsetLeft)
+    var mouseY = e.pageY - (canvas.clientTop + canvas.offsetTop)
+    for (var i = 0; i < buttons.length; i ++){
+      if (buttons[i].mousehover(mouseX, mouseY)){
+        buttons[i].onClick()
+      }
+    }
+  }
 })
 
 function endgame() { ///////////////////////////////////////// FIX
-  console.log("pop1111111111111111111");
   cancelAnimationFrame(update);
   dead = true
 }
+
+logo.addEventListener("load", () => {
+  ctx.drawImage(logo, canvas.width/2, 0, blocksize * 10, blocksize * 10)
+});
+
 
 // function makeSpike(x, y) {
 //   base_image = new Image();
